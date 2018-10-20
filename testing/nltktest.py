5610d9283baf616d577f6651cd4e16e608eb66c9
beerbottles <>
2018-10-20 18:18:42 +0530

    added testing folder

---
+++ testing/nltktest.py
@@ -0,0 +1,43 @@
+"""
+1. remove stop words
+2. make list of synonyms of user input
+3. get all non stop words from website if exists
+and compare with synonym list of user side
+
+4. also create antonym list
+"""
+
+import nltk
+import re
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+#nltk.download() download has been complete
+#sentence="This is any delimeter, by default it is space."
+#tokens=nt.word_tokenize(sentence)
+#print(sent_tokenize(sentence))
+#for i in range(0, len(tokens)):
+#    print(tokens[i])
+sent="Hello world! Where are you? I am fine. Good night. Today's Headlines are"
+#p=re.compile('[a-z]')
+print(sent_tokenize(sent))
+print(word_tokenize(sent)) 
+print(sent.split())
+
+#convert to lower character
+lowerWords=word_tokenize(sent)
+for i in range(0, len(lowerWords)):
+    lowerWords[i]=lowerWords[i].lower()
+
+#to remove punctuation marks
+newWords=[]
+for i in range(0, len(lowerWords)):
+    s=re.search(r'[a-z0-9\']+', lowerWords[i])
+    if s:
+        newWords.append(lowerWords[i])
+
+
+
+"""
+problem with split is that it takes continous letters aas words
+so 'when?' is also considered a word(inlcuding '?') and not only when
+"""
---
+++ testing/test.py
@@ -0,0 +1,31 @@
+import re
+"""
+d matches a digit
+"""
+
+p=re.compile('\d+')
+#print(p.match('hello93'))
+#m=p.match('23hello 34')
+#print(m.group())
+s=p.search('23 helllo 34')
+print(s.group()) #search stops after matching
+
+
+#use this instead of word_tokenise
+matches=re.findall(r'[a-zA-Z0-9\']+', "Hello world! Where are you? I am fine. Good night. Today's Headlines are")
+for match in matches:
+    print(match.lower())
+
+
+
+
+"""
+inpt="aaaaabbbbcccc"
+word="Hello this word has spaces"
+print(inpt.strip('a'))
+print(word.strip('this'))
+
+wordls=word.split()
+for i in range(0, len(wordls)):
+    print(wordls[i])
+"""
